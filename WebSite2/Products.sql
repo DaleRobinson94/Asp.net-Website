a97d0bc00c1ff86369a97467f145c929b872e127
DaleRobinson94 <dalerobinson1994@gmail.com>
2016-09-04 21:19:33 +0100

    Package

---
+++ WebSite2/Products.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Products]
+(
+	[ProductID] CHAR(10) NOT NULL PRIMARY KEY, 
+    [ProductName] VARCHAR(50) NOT NULL, 
+    [ProductDescription] VARCHAR(500) NOT NULL, 
+    [ProductImage] VARCHAR(50) NOT NULL, 
+    [ProductPrice] MONEY NULL
+)
---
+++ WebSite2/userInformation.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[userInformation] (
+    [ID]       NVARCHAR(50)        NOT NULL,
+    [Username] NCHAR (50) NOT NULL,
+    [Email]    NCHAR (50) NOT NULL,
+    [Password] NCHAR (50) NOT NULL, 
+    CONSTRAINT [PK_userInformation] PRIMARY KEY ([ID])
+);
+
